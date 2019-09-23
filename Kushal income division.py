3fd6e3f1e7c53c6da2339564b86845bfb1736eff
Kushal861 <55680903+Kushal861@users.noreply.github.com>
2019-09-23 11:13:35 +0530

    Add files via upload
    JUST A SIMPLE CODE IN PYTHON TO DIVIDE YOUR INCOME FOR DIFFERENT PURPOSE.

---
+++ Kushal income division.py
@@ -0,0 +1,11 @@
+a=int(input("Enter your income :"))
+b=(a*25)/100
+c=(15*a)/100
+d=(30*a)/100
+e=(10*a)/100
+f=(20*a)/100
+print("25% income for grocery:",format(b))
+print("15% income for health and clinics :",format(c))
+print("30% income for savings ",format(d))
+print("10% income for entertainment :",format(e))
+print("20% income for bills payment :",format(f))
